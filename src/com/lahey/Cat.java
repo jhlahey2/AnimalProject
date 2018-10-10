a5f63f2ecbbe47196fc8d543492eb1dc765d1b14
samuelsong2018 <jhlahey@protonmail.com>
2018-10-10 13:40:37 -0400

    first commit

---
+++ src/com/lahey/Cat.java
@@ -0,0 +1,11 @@
+package com.lahey;
+
+public class Cat extends Animal {
+
+
+    //override  Animal.sleeps() method
+    @Override
+    public String sleeps() {
+        return "The cat is sleeping";
+    }
+}
--- src/com/lahey/Main.java
+++ src/com/lahey/Main.java
@@ -3,8 +3,6 @@ package com.lahey;
 public class Main {
 
     public static void main(String[] args) {
-
-
         Animal dog = new Animal("dog");
         System.out.println(dog.getAnimalName());
         System.out.println(dog.eats());
@@ -12,7 +10,19 @@ public class Main {
         System.out.println(dog.myThoughts());
 
 
+        Animal a = new Animal();
+        System.out.println(a.eats());
+        System.out.println(a.sleeps());
 
+        Cat c = new Cat();
+        System.out.println(c.eats());
+        System.out.println(c.sleeps());
+
+
+        Bird b = new Bird();
+        System.out.println(b.eats());
+        System.out.println(b.sleeps());
+        System.out.println(b.fly());
 
 
 
